95374a2a39cf5a315600b6660e1e0db192b428ff
Darshan <darshantank3@gmail.com>
2017-08-06 19:17:17 +0530

    Added arts and ideas

--- artsandideas/admin.py
+++ artsandideas/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import ArtsAndIdeasGenre, ArtsAndIdeasEvent
 
 # Register your models here.
+admin.site.register(ArtsAndIdeasEvent)
+admin.site.register(ArtsAndIdeasGenre)
