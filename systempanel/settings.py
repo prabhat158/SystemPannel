46a22cb0e725e22b2e90a5b423da16b074fffe5c
Darshan <darshantank3@gmail.com>
2017-08-08 12:33:45 +0530

    Added mysql and requirements file

--- systempanel/settings.py
+++ systempanel/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'systempanel.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'MI2k17',
+        'USER': 'root',
+        'PASSWORD': 'cooldude1',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
