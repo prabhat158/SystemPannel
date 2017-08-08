148bcc4b77d92acf8d396d8bee46aaef12f3bf55
Darshan <darshantank3@gmail.com>
2017-08-08 11:54:22 +0530

    Added cors middleware

--- events/urls.py
+++ events/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^events/$', views.Events.as_view()),
-    url(r'^register/(?P<event_id>[0-9]+)/$',
+    url(r'^register/(?P<event_id>[0-9]+)$',
         views.Compireg.as_view()),
     url(r'^competitions/(?P<fb_id>[0-9]+)/$',
         views.UserCompetitions.as_view()),
